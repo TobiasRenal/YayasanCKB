0c8743ae837a9c8b3617ae84be513e2581eb7f8b
TobiasRenal <1872015@maranatha.ac.id>
2021-06-20 18:13:17 +0700

    initial

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/sejarah', function () {
+    return view('sejarah');
+});
+
+Route::get('/inggris', function () {
+    return view('inggris');
+});
+
+Route::get('/komputer', function () {
+    return view('komputer');
+});
+
+Route::get('/musik', function () {
+    return view('musik');
+});
+Route::get('/kober', function () {
+    return view('kober');
+});
+Route::get('/taekwondo', function () {
+    return view('taekwondo');
+});
+Route::get('/karate', function () {
+    return view('karate');
+});
+Route::get('/personalia', function () {
+    return view('personalia');
+});
